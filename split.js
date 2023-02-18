6be5c328c738312879e7eb01d0e3b9572a0a0d3a
ThetisLake <68162082+ericpien@users.noreply.github.com>
2023-02-17 22:09:11 -0800

    initial upload of version 0.1

---
+++ split.js
@@ -0,0 +1,81 @@
+
+console.log('foo');
+
+document.getElementById("split").addEventListener("click", split());
+
+async function split() {
+    const activeTab = await getActiveTab();
+
+    console.log(activeTab.url);
+
+    // var data = `<!DOCTYPE html>
+    // <html>
+    // <head>
+    //   <title>Two Iframe Windows Example</title>
+    //   <link rel="stylesheet" href="main.css">
+    // </head>
+    // <body>
+    //   <iframe id="iframe1" src="`+ activeTab.url +`" scrolling="yes"></iframe>
+    //   <iframe id="iframe2" src="`+ activeTab.url +`"></iframe>
+    // </body>
+    // </html>`;
+
+    var data = `<!DOCTYPE html> <html> <head> <title>Two Iframe Windows Example</title> <link rel="stylesheet" href="main.css"> </head> <body><iframe id="iframe1" src="`+ activeTab.url +`" scrolling="yes"></iframe><iframe id="iframe2" src="`+ activeTab.url +`"></iframe></body></html>`;
+
+    var tester = `<html>
+    <head>
+      <title>A Simple HTML Document</title>
+    </head>
+    <body>
+      <p>This is a very simple HTML document</p>
+      <p>It only has two paragraphs</p>
+    </body>
+  </html>`;
+  
+
+    console.log(tester);
+
+    chrome.scripting.executeScript({
+        args: [activeTab.url],
+        target: {
+            tabId: activeTab.id,
+        }, 
+        func: getHTML,
+        // func: () => {    
+        //     document.write(`<html>
+        //     <head>
+        //       <title>A Simple HTML Document</title>
+        //     </head>
+        //     <body>
+        //       <p>This is a very simple HTML document</p>
+        //       <p>It only has two paragraphs</p>
+        //     </body>
+        //   </html>`);
+        // },
+    });
+}
+
+function getActiveTab() {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const activeTab = tabs[0];
+            resolve(activeTab);
+        });
+    });
+}
+
+function getHTML(url) {
+    let data = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Two Iframe Windows Example</title>
+      <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+      <iframe id="iframe1" src="`+ url +`" scrolling="yes"></iframe>
+      <iframe id="iframe2" src="`+ url +`"></iframe>
+    </body>
+    </html>`
+
+    document.write(data);
+}
